7e0fb8581e4998daef9532289a4168c1987338ff
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2022-05-22 19:12:36 -0300

    Organizacion en 3 capas + paquetes solicitados

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,5 +1,13 @@
 package main;
 
-public class Principal {
+import negocio.PersonaNegocio;
+import negocioImpl.PersonaNegocioImpl;
+import presentacion.controlador.Controlador;
+import presentacion.vista.VentanaPrincipal;
 
+public class Principal {
+	VentanaPrincipal vista = new VentanaPrincipal();
+	PersonaNegocio negocio = new PersonaNegocioImpl();
+	//Controlador controlador = new Controlador(vista, negocio);
+	//controlador.;
 }
---
+++ src/presentacion/controlador/Controlador.java
@@ -0,0 +1,29 @@
+package presentacion.controlador;
+
+import java.awt.event.ActionListener;import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import negocio.PersonaNegocio;
+import entidad.Persona;
+public class Controlador  implements ActionListener{
+
+
+	
+	private PersonaNegocio pNeg;
+	private ArrayList<Persona> personasEnTabla;
+	
+	public void inicializar()
+	{
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+	
+	
+	
+
+}
