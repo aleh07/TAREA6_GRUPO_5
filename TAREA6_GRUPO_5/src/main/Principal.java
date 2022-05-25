0e4248c464d74898ae8d927b6b71f855b7ca0ce6
Herrera Alejandro <81126261+aleh07@users.noreply.github.com>
2022-05-25 18:15:38 -0300

    abriendo desde el menu personas agregar y eliminar

---
+++ TAREA6_GRUPO_5/src/main/Principal.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.awt.EventQueue;
+
+import negocio.PersonaNegocio;
+import negocioImpl.PersonaNegocioImpl;
+import presentacion.controlador.Controlador;
+import presentacion.vista.VentanaPrincipal;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+			
+			VentanaPrincipal vista = new VentanaPrincipal();
+			PersonaNegocio negocio = new PersonaNegocioImpl();
+			Controlador controlador = new Controlador(vista,negocio);
+			controlador.inicializar();
+		}
+}
---
+++ TAREA6_GRUPO_5/src/presentacion/controlador/Controlador.java
@@ -0,0 +1,79 @@
+package presentacion.controlador;
+
+import java.awt.event.ActionListener;import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import negocio.PersonaNegocio;
+import presentacion.vista.VentanaPrincipal;
+import presentacion.vista.VentanaAgregar;
+import presentacion.vista.VentanaEliminar;
+import presentacion.vista.VentanaListar;
+import presentacion.vista.VentanaModificar;
+import entidad.Persona;
+
+public class Controlador  implements ActionListener{
+
+
+	
+	private PersonaNegocio pNeg;
+	private ArrayList<Persona> personasEnTabla;
+	private VentanaPrincipal ventanaPrincipal;
+	private VentanaAgregar ventanaAgregar;
+	private VentanaEliminar ventanaEliminar;
+	private VentanaListar ventanaListar;
+	private VentanaModificar ventanaModificar;
+	
+	public Controlador(VentanaPrincipal vista, PersonaNegocio pNeg)
+	{
+		//Guardo todas las instancias que recibo en el constructor
+		this.ventanaPrincipal = vista;
+		this.pNeg = pNeg;
+		
+		//Instancio los paneles
+		this.ventanaAgregar = new VentanaAgregar();
+		this.ventanaEliminar = new VentanaEliminar();
+		
+		//Enlazo todos los eventos
+		
+		//Eventos menu del Frame principal llamado Ventana
+		this.ventanaPrincipal.getAgregar().addActionListener(a->EventoClickMenu_AbrirPanel_AgregarPersona(a));
+		this.ventanaPrincipal.getEliminar().addActionListener(a->EventoClickMenu_AbrirPanel_EliminarPersona(a));
+		
+		}
+	
+
+	public void inicializar()
+	{
+		this.ventanaPrincipal.setVisible(true);;
+	}
+	
+	
+	
+	public void  EventoClickMenu_AbrirPanel_AgregarPersona(ActionEvent a)
+	{		
+		
+		ventanaPrincipal.getContentPane().removeAll();
+		ventanaPrincipal.getContentPane().add(ventanaAgregar);
+		ventanaPrincipal.getContentPane().repaint();
+		ventanaPrincipal.getContentPane().revalidate();
+	}
+
+	public void  EventoClickMenu_AbrirPanel_EliminarPersona(ActionEvent a)
+	{		
+		
+		ventanaPrincipal.getContentPane().removeAll();
+		ventanaPrincipal.getContentPane().add(ventanaEliminar);
+		ventanaPrincipal.getContentPane().repaint();
+		ventanaPrincipal.getContentPane().revalidate();
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+		
+	
+	
+	
+
+}
