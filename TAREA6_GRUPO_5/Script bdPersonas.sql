2a70b3b4d9def1867e0cbd76b8542ac76130366b
Pablo Ezequiel Caero <pablo_ieme@hotmail.com>
2022-05-26 16:51:35 -0300

    Se elimina proyecto duplicado

--- TAREA6_GRUPO_5/Script bdPersonas.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE `bdPersonas`;
-USE bdPersonas;
-CREATE TABLE `Personas`
-(
-`Dni` varchar(20) NOT NULL,
-`Nombre` varchar(45) NOT NULL,
-`Apellido` varchar(45) NOT NULL,
-PRIMARY KEY (`Dni`));
